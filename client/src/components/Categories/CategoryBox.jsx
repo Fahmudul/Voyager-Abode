2ab2124a2fe8e4ea11e30e0e0f0574824cfb0b2b
Fahmudul Hassan <fahmudul7890@email.com>
2024-05-22 12:32:19 +0600

    implemented basic functionalities like view details. page login signup navbar

--- client/src/components/Categories/CategoryBox.jsx
+++ client/src/components/Categories/CategoryBox.jsx
@@ -1,6 +1,17 @@
-import PropTypes from 'prop-types'
-
+import PropTypes from "prop-types";
+import queryString from "query-string";
+import { useNavigate, useNavigation } from "react-router-dom";
 const CategoryBox = ({ label, icon: Icon }) => {
+  const navigate = useNavigate();
+  const handleRoom = () => {
+    const category = { category: label };
+    const url = queryString.stringifyUrl({
+      url: "/",
+      query: category,
+    });
+    // console.log(url);
+    navigate(url);
+  };
   return (
     <div
       className={`flex 
@@ -13,16 +24,17 @@ const CategoryBox = ({ label, icon: Icon }) => {
   hover:text-neutral-800
   transition
   cursor-pointer`}
+      onClick={() => handleRoom(label)}
     >
       <Icon size={26} />
-      <div className='text-sm font-medium'>{label}</div>
+      <div className="text-sm font-medium">{label}</div>
     </div>
-  )
-}
+  );
+};
 
 CategoryBox.propTypes = {
   label: PropTypes.string,
   icon: PropTypes.elementType,
-}
+};
 
-export default CategoryBox
+export default CategoryBox;
